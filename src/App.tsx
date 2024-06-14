d1cb046cf2ff303bc4c46117899c751bde3fc989
Alessandro Rios <drios28@outlook.es>
2024-06-14 16:19:42 -0500

    chore: Update app info, title and description, add favicon and manifest.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { LayersIcon } from 'lucide-react'
+
 export default function App () {
   return (
-    <div>
+    <div className='p-5 min-h-dvh flex flex-col gap-10'>
+      <header className='flex gap-2 items-center'>
+        <LayersIcon className='w-6 h-6' />
+        <span>DALPOS</span>
+      </header>
+
       <main>
-        <h1>DALPOS</h1>
       </main>
     </div>
   )
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = 'Dalpos'
+export const APP_DESCRIPTION = 'DALPOS es una aplicación web que te permite controlar tu negocio de manera sencilla y eficiente, lleva el control de tus productos, inventario y ventas de manera fácil y rápida.'
