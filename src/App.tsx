9fcb084a35daeaf83be36c615a235be9710a5a48
Alessandro Rios <drios28@outlook.es>
2024-06-14 16:24:25 -0500

    chore: add placeholder text.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { LayersIcon } from 'lucide-react'
 
 export default function App () {
   return (
-    <div className='p-5 min-h-dvh flex flex-col gap-10'>
-      <header className='flex gap-2 items-center'>
+    <div className='flex flex-col gap-10 p-5 min-h-dvh'>
+      <header className='flex items-center gap-2'>
         <LayersIcon className='w-6 h-6' />
         <span>DALPOS</span>
       </header>
 
-      <main>
+      <main className='flex flex-col items-center justify-center grow'>
+        <h1 className='text-6xl font-black sm:text-7xl md:text-8xl lg:text-9xl text-balance'>
+          COMING SOON...
+        </h1>
       </main>
     </div>
   )
